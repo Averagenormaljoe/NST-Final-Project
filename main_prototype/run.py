e5166526b3671a021d01b8a3fd25d65b7c1b7cfe
layo <layo_a@live.com>
2025-05-02 12:15:49 +0100

    feat: updated the structure of the project to be more clear

---
+++ main_prototype/run.py
@@ -0,0 +1,14 @@
+import os
+
+
+notebook_path = "book_prototype.ipynb"  
+
+
+command = f"jupyter nbconvert --execute --to notebook --inplace {notebook_path}"
+
+
+success = os.system(command)
+if success != 0:
+    print("Notebook execution completed!")
+else:
+    print("Notebook execution failed!")
