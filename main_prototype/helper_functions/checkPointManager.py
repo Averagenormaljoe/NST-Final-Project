32beb70e27a01adc7008df4d8a82ef5d8e0a71bc
layo <layo_a@live.com>
2025-06-27 07:20:56 +0100

    feat: added checkPointManager to the project

---
+++ main_prototype/helper_functions/checkPointManager.py
@@ -0,0 +1,20 @@
+class checkPointManager:
+    def __init__(self, combination_image, optimizer : str, checkpoint_dir : str, folder_path : str):
+        self.optimizer = optimizer
+        self.combination_image = combination_image
+        self.checkpoint_dir = checkpoint_dir
+        
+        self.folder_path =  folder_path
+        self.checkpoint = tf.train.Checkpoint(optimizer=optimizer, combination_image=combination_image)
+        self.manager = tf.train.CheckpointManager(self.checkpoint, directory=checkpoint_dir, max_to_keep=5)
+
+    def save(self):
+        return self.manager.save()
+    
+    def save_checkpoint(self, step,checkpoint_prefix):
+        self.checkpoint.save(file_prefix=checkpoint_prefix)
+    
+    def setup(self):
+        create_dir(self.folder_path)
+        checkpoint_dir = "./checkpoints"
+        create_dir(checkpoint_dir)
