e49ac329e8a7403fba24818bea6a0fb50b3f8fdf
layo <layo_a@live.com>
2025-06-28 05:39:54 +0100

    feat: added functions for tracking power usage in the project

---
+++ shared_utils/device.py
@@ -0,0 +1,36 @@
+import subprocess
+
+import comm
+def get_gpu_usage():
+    try:
+        command = ['nvidia-smi', '--query-gpu=utilization.gpu', '--format=csv,noheader,nounits']
+        return process_command(command)
+    except:
+        pass
+   
+
+def get_memory_usage():
+    try:
+        command = ['nvidia-smi', '--query-gpu=memory.used', '--format=csv,noheader,nounits']
+        return process_command(command)
+    except:
+        pass
+    
+def get_gpu_temperature():
+    try:
+        command = ['nvidia-smi', '--query-gpu=temperature.gpu', '--format=csv,noheader,nounits']
+        return process_command(command)
+    except:
+        pass
+def get_gpu_power_usage():
+    try:
+        command = ['nvidia-smi', '--query-gpu=power.draw', '--format=csv,noheader,nounits']
+        return process_command(command)
+    except:
+        pass
+
+
+
+def process_command(command):
+    output = subprocess.check_output(command)
+    return int(output.decode('utf-8').strip())
