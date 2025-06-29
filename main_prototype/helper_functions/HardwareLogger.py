8a757c81d359d13258542e4d88e29bf0aefe94f3
layo <layo_a@live.com>
2025-06-29 19:38:31 +0100

    feat: updated checkpoint manager and added a hardware logger script

---
+++ main_prototype/helper_functions/HardwareLogger.py
@@ -0,0 +1,63 @@
+from psutil import disk_usage
+class HardwareLogger:
+    def __init__(self,verbose : int = 0):
+        self.verbose = verbose
+        self.total_wall_time = time.time() 
+        self.total_time_cpu = time.process_time()
+        self.start_time_cpu = time.process_time()
+        self.start_time_wall = time.time()
+        self.log_data = {}
+    def append(self,key,item):
+        self.log_data.setdefault(key, []).append(item)
+    
+    def log_hardware(self):
+        gpu = get_gpu_usage()
+        if gpu is not None:
+            self.append("gpu",gpu)
+        ram = psutil.virtual_memory().percent
+        cpu = psutil.cpu_percent(interval=1)
+        disk = disk_usage('/').percent
+            
+        # append hardware usage statistics
+        self.append("ram",ram)
+        self.append("disk",disk)
+        self.append("cpu",cpu)
+    def log_loss(self,loss,i):
+        self.append("loss",loss)
+        self.append("iterations",i)
+        if self.verbose > 0:
+            print(f"Iteration {i}: loss={loss:.2f}")
+        
+    def record_time_diff(self):
+        end_time_cpu = time.process_time()  
+        end_time_wall = time.time()  
+        cpu_time = end_time_cpu - self.start_time_cpu  
+        wall_time = end_time_wall - self.start_time_wall
+        return cpu_time, wall_time
+    
+    def log_process_time(self,cpu_time,wall_time):
+        self.append("cpu time",cpu_time)
+        self.append("wall time",wall_time)
+        if self.verbose > 0:
+            print("CPU times in seconds: {:.2f}".format(cpu_time))
+            print("Wall time in seconds: {:.2f}".format(wall_time))
+    
+    def log_end_check(self):
+        cpu_time, wall_time = self.record_time_diff()
+        self.log_process_time(cpu_time,wall_time)
+        self.reset_start_time()
+        
+    def reset_start_time(self):
+        self.start_time_cpu = time.process_time()
+        self.start_time_wall = time.time()
+    def on_training_end(self):
+        end_time_wall = time.time()
+        end_time_cpu = time.process_time()
+        end_total_wall_time = end_time_wall - self.total_wall_time
+        end_total_time_cpu = end_time_cpu - self.total_time_cpu
+        if self.verbose > 0:
+            print(f"Total wall time: {end_total_wall_time} seconds")
+            print(f"Total CPU time: {end_total_time_cpu} seconds")
+    def get_log(self):
+        return self.log_data
+        
--- main_prototype/helper_functions/checkPointManager.py
+++ main_prototype/helper_functions/checkPointManager.py
@@ -1,19 +1,18 @@
+from shared_utils.helper import create_dir
+import tensorflow as tf
+
 class checkPointManager:
     def __init__(self, combination_image, optimizer : str, checkpoint_dir : str, folder_path : str):
         self.optimizer = optimizer
         self.combination_image = combination_image
         self.checkpoint_dir = checkpoint_dir
-        
         self.folder_path =  folder_path
         self.checkpoint = tf.train.Checkpoint(optimizer=optimizer, combination_image=combination_image)
         self.manager = tf.train.CheckpointManager(self.checkpoint, directory=checkpoint_dir, max_to_keep=5)
-
-    def save(self):
+    def get_manager(self):
         return self.manager.save()
-    
     def save_checkpoint(self, step,checkpoint_prefix):
         self.checkpoint.save(file_prefix=checkpoint_prefix)
-    
     def setup(self):
         create_dir(self.folder_path)
         checkpoint_dir = "./checkpoints"
