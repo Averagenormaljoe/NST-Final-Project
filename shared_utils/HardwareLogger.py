e87c6ee86eabae0312dfa5ee9618abb3289f33c5
layo <layo_a@live.com>
2025-07-16 08:02:38 +0100

    feat: updated 'HardwareLogger.py' to handle out of scope errors for the tf hardware logger class

--- shared_utils/HardwareLogger.py
+++ shared_utils/HardwareLogger.py
@@ -83,7 +83,7 @@ class TFHardwareLogger(HardwareLogger):
         self.total_duration = 0
  
     def train_end(self):
-        self.end_time = tf.timestamp()
+        self.end_time = tf.timestamp().numpy()
         if self.start_time is not None and self.end_time is not None:
             train_duration = self.end_time - self.start_time
             self.append("train duration",train_duration)
@@ -95,5 +95,5 @@ class TFHardwareLogger(HardwareLogger):
         self.log_hardware()
         
     def reset_tf(self):
-        self.start_time = tf.timestamp()
+        self.start_time = tf.timestamp().numpy()
         self.end_time = None
