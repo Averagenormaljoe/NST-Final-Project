57e383faf27f9179505610fc198720229dfb12ff
layo <layo_a@live.com>
2025-07-29 17:59:53 +0100

    feat: added specific function for google colab

---
+++ AdaIN/helper_functions/colab_functions.py
@@ -0,0 +1,9 @@
+def download_libraries():
+    !pip install torch_fidelity
+    !pip install pytorch_msssim
+    !pip install lpips
+    !pip install tf2onnx
+    !pip install coremltools
+    !pip install tensorflowjs
+    !pip uninstall tensorflow -y
+    !pip install tensorflow[and-cuda]
