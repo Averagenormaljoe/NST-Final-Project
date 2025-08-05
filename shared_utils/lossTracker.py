7237b041e7e9db77da3c9d1a0443234b9f89d756
layo <layo_a@live.com>
2025-08-05 07:29:52 +0100

    feat: removed 'LossTracker' from 'shared_utils/lossTracker.py' due to being unused

--- shared_utils/lossTracker.py
+++
@@ -1,9 +0,0 @@
-class LossTracker:
-    def __init__(self):
-        self.losses = {}
-
-    def add(self, key, loss):
-        self.losses.setdefault(key, []).append(loss)
-    def get_loss(self, key):
-        return self.losses.get(key, [])
-
