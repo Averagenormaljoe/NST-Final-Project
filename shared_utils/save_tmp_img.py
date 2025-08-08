5522bf149b2b8e8e7f3373420792730d8c69efd3
layo <layo_a@live.com>
2025-08-08 07:20:52 +0100

    feat: added functionality to save a tensorflow image in eagar execution mode

--- shared_utils/save_tmp_img.py
+++ shared_utils/save_tmp_img.py
@@ -1,16 +1,18 @@
 import os
 import shutil
 from PIL import Image
-def save_tmp_img(image, folder, prefix="tmp", return_img=False):
+import tensorflow as tf
+def save_tmp_img(image, folder, prefix="tmp", return_img=False, save_tensor=False):
     dir_path = f"/{prefix}/{folder}"
     if os.path.exists(dir_path):
         shutil.rmtree(dir_path)
     os.makedirs(dir_path, exist_ok=True)
     img_name = "img_dummy.png"
     img_path = os.path.join(dir_path, img_name)
-    numpy_image = image.squeeze().astype("uint8")
-    im = Image.fromarray(numpy_image)
-    im.save(img_path)
+    if save_tensor:
+        save_in_tensor(image, img_path)
+    else:
+        save_in_numpy(image, img_path)
     if return_img:
         return img_path
     return dir_path
@@ -18,3 +20,14 @@ def save_tmp_img(image, folder, prefix="tmp", return_img=False):
 def destroy_tmp_img(path):
     if os.path.exists(path):
         shutil.rmtree(path)
+            
+def save_in_numpy(image, img_path):
+    numpy_image = image.squeeze().astype("uint8")
+    im = Image.fromarray(numpy_image)
+    im.save(img_path)
+    
+def save_in_tensor(img_tensor,path):
+    img_uint8 = tf.image.convert_image_dtype(img_tensor, dtype=tf.uint8)
+    encoded = tf.io.encode_png(img_uint8)
+    tf.io.write_file(path, encoded) 
+         
