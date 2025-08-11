0bc01fc5c277a97508e97655f35b5a33b9420769
layo <layo_a@live.com>
2025-08-11 13:00:00 +0100

    feat: added activation layer to 'forward_feed/CIN/ConvInstanceLayer.py'

--- forward_feed/CIN/ConvInstanceLayer.py
+++ forward_feed/CIN/ConvInstanceLayer.py
@@ -2,11 +2,12 @@ import tensorflow as tf
 from forward_feed.CIN.CIN import ConditionalInstanceNormalization
 from model_components.ReflectionPadding2D import ReflectionPadding2D
 class ConvInstanceLayer(tf.keras.layers.Layer):
-    def __init__(self, num_style, in_ch, out_ch,filters, kernel_size, strides=1, **kwargs):
+    def __init__(self, num_style, in_ch, out_ch,filters, kernel_size, strides=1, activation='relu', **kwargs):
         super(ConvInstanceLayer, self).__init__(**kwargs)
         self.padding = ReflectionPadding2D([k // 2 for k in kernel_size])
         self.conv2d = tf.keras.layers.Conv2D(filters, kernel_size, strides)
         self.cin = ConditionalInstanceNormalization(num_style=num_style)
+        self.activation = tf.keras.layers.Activation(activation)
 
     def call(self, inputs,style_codes):
         x = self.padding(inputs)
