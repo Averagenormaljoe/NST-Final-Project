268e354ae36e0d29894fc1d14f38f58d95d36308
layo <layo_a@live.com>
2025-08-11 13:13:00 +0100

    feat: implementing 'forward_feed/CIN/ConvInstanceLayer.py'

--- forward_feed/CIN/ConvInstanceLayer.py
+++ forward_feed/CIN/ConvInstanceLayer.py
@@ -2,10 +2,13 @@ import tensorflow as tf
 from forward_feed.CIN.CIN import ConditionalInstanceNormalization
 from model_components.ReflectionPadding2D import ReflectionPadding2D
 class ConvInstanceLayer(tf.keras.layers.Layer):
-    def __init__(self, num_style, in_ch, out_ch,filters, kernel_size, strides=1, activation='relu', **kwargs):
+    def __init__(self, num_style, filters, kernel_size, strides=1, activation='relu', **kwargs):
         super(ConvInstanceLayer, self).__init__(**kwargs)
-        self.padding = ReflectionPadding2D([k // 2 for k in kernel_size])
-        self.conv2d = tf.keras.layers.Conv2D(filters, kernel_size, strides)
+        pad = [k // 2 for k in kernel_size]
+        kernel_size_tuple = (kernel_size, kernel_size)
+        self.padding = ReflectionPadding2D(pad)
+        
+        self.conv2d = tf.keras.layers.Conv2D(filters, kernel_size_tuple, strides)
         self.cin = ConditionalInstanceNormalization(num_style=num_style)
         self.activation = tf.keras.layers.Activation(activation)
 
