4d47fa74d4d2670d0b18dfdd924d75d545f7ffcf
layo <layo_a@live.com>
2025-08-11 09:47:08 +0100

    feat: refactoring the forward feed notebook

--- forward_feed/CIN/CIN.py
+++ forward_feed/CIN/CIN.py
@@ -4,6 +4,12 @@ from model_components.ReflectionPadding2D import ReflectionPadding2D
 class ConditionalInstanceNormalization(tf.keras.layers.Layer):
     def __init__(self, **kwargs):
         super(ConditionalInstanceNormalization, self).__init__(**kwargs)
+  
+
+
+   
+
+
 
     def call(self, inputs, style_vector):
         batch, rows, cols, channels = [i for i in inputs.get_shape()]
@@ -18,7 +24,7 @@ class ConditionalInstanceNormalization(tf.keras.layers.Layer):
         for i in range(len(style_vector)):
             shift.append(tf.get_variable('shift', shape=var_shape, initializer=tf.constant_initializer(0.)))
             scale.append(tf.get_variable('scale', shape=var_shape, initializer=tf.constant_initializer(1.)))
-        normalized = (inputs - mu) / tf.sqrt(var + epsilon) **(.5)
+        normalized = (inputs - mu) / tf.sqrt(var + epsilon)
          
         idx = [i for i, x in enumerate(style_vector) if not x == 0]
         
@@ -28,16 +34,3 @@ class ConditionalInstanceNormalization(tf.keras.layers.Layer):
         return output
     
     
-class ConvInstanceLayer(tf.keras.layers.Layer):
-    def __init__(self, num_style, in_ch, out_ch,filters, kernel_size, strides=1, **kwargs):
-        super(ConvInstanceLayer, self).__init__(**kwargs)
-        self.padding = ReflectionPadding2D([k // 2 for k in kernel_size])
-        self.conv2d = tf.keras.layers.Conv2D(in_ch, out_ch,filters, kernel_size, strides)
-        self.cin = ConditionalInstanceNormalization(num_style=num_style)
-
-    def call(self, inputs,style_codes):
-        x = self.padding(inputs)
-        x = self.conv(x)
-        x = self.cin(x, style_codes)
-        x = self.activation(x)
-        return x
---
+++ forward_feed/CIN/ConvInstanceLayer.py
@@ -0,0 +1,13 @@
+class ConvInstanceLayer(tf.keras.layers.Layer):
+    def __init__(self, num_style, in_ch, out_ch,filters, kernel_size, strides=1, **kwargs):
+        super(ConvInstanceLayer, self).__init__(**kwargs)
+        self.padding = ReflectionPadding2D([k // 2 for k in kernel_size])
+        self.conv2d = tf.keras.layers.Conv2D(in_ch, out_ch,filters, kernel_size, strides)
+        self.cin = ConditionalInstanceNormalization(num_style=num_style)
+
+    def call(self, inputs,style_codes):
+        x = self.padding(inputs)
+        x = self.conv(x)
+        x = self.cin(x, style_codes)
+        x = self.activation(x)
+        return x
