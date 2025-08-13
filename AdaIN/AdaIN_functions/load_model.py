ec1f67a5af12f965b3e7c705e52a8ea850a81ac9
layo <layo_a@live.com>
2025-08-13 07:32:32 +0100

    feat: added 'review_tensorboard_results' function back as a magic command in 'AdaIN/StyleMotion_adain.ipynb'

--- AdaIN/AdaIN_functions/load_model.py
+++ AdaIN/AdaIN_functions/load_model.py
@@ -1,18 +1,14 @@
 import keras
 import tensorflow as tf
 
-
 def load_keras_model():
-    """Loads the saved model."""
     encoder = keras.models.load_model("encoder")
     decoder = keras.models.load_model("decoder")
     loss_net = keras.models.load_model("loss_net")
     model = keras.models.load_model("model")
     return encoder, decoder, loss_net, model
 
-
 def load_legacy_model():
-    """Loads the legacy saved model."""
     encoder = keras.models.load_model("encoder.h5")
     decoder = keras.models.load_model("decoder.h5")
     loss_net = keras.models.load_model("loss_net.h5")
@@ -20,7 +16,6 @@ def load_legacy_model():
     return encoder, decoder, loss_net, model
 
 def load_pb_model():
-    """Loads the saved model from the saved_model_pb directory."""
     model_dir = "saved_model_pb"
     model = tf.saved_model.load(model_dir)
     return model
--- AdaIN/AdaIN_functions/tensorboard_helper.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-def review_tensorboard_results(show_tensorboard: bool = True):
-    # display the tensorboard results from the training.
-    if show_tensorboard:
-        port = 6006
-        command = f"%tensorboard --logdir logs/fit --port {port}"
-        split_command = command.split()
-        subprocess.run(split_command, check=True)
