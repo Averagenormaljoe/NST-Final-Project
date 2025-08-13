de5e6b5f24d9bd42324f6d605ed5fe908aae9839
layo <layo_a@live.com>
2025-08-13 05:26:55 +0100

    feat: refactored the 'load_tf_image' in the forward feed model and the 'decode_and_resize' function of the AdaIN model due to them using simlilar functionality

--- AdaIN/AdaIN_functions/helper.py
+++ AdaIN/AdaIN_functions/helper.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from shared_utils.image_utils import tf_decode_jpeg
 IMAGE_SIZE = (224, 224)
 def decode_and_resize(image_path):
     """Decodes and resizes an image from the image file path.
@@ -9,10 +10,8 @@ def decode_and_resize(image_path):
     Returns:
         A resized image.
     """
-    image = tf.io.read_file(image_path)
-    channels : int = 3
-    image = tf.image.decode_jpeg(image, channels=channels)
-    image = tf.image.convert_image_dtype(image, dtype="float32")
+    image = tf_decode_jpeg(image_path)
+    image = tf.image.convert_image_dtype(image, tf.float32)
     image = tf.image.resize(image, IMAGE_SIZE)
     return image
 
