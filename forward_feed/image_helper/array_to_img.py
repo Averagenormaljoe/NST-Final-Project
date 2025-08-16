2841393ccd268dab09af463fd7b79aa304c70523
layo <layo_a@live.com>
2025-08-16 17:59:13 +0100

    feat: added the 'update_optimizer' function to the project

--- forward_feed/image_helper/array_to_img.py
+++ forward_feed/image_helper/array_to_img.py
@@ -2,7 +2,9 @@ import numpy as np
 from PIL import Image
 def array_to_img(array):
     array = np.array(array, dtype=np.uint8)
+    array = np.squeeze(array)
     if np.ndim(array) > 3:
         assert array.shape[0] == 1
         array = array[0]
+        
     return Image.fromarray(array)
