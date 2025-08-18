b940993e3a26737c594508fc1267f4e46973570c
layo <layo_a@live.com>
2025-08-18 06:35:17 +0100

    feat: fixed the 'download_libraries' adding target for uninstall

--- shared_utils/colab_functions.py
+++ shared_utils/colab_functions.py
@@ -9,12 +9,12 @@ def download_libraries(path : str = "/content/drive/MyDrive/Library"):
         "pip install tensorflowjs",
         "pip uninstall tensorflow -y",
         "pip install tensorflow[and-cuda]",
-        "pip install --upgrade tensorflow_decision_forests"
+
     ]
     for cmd in pip_commands:
         command = cmd.split()
-        if path:
-            command.insert(2, f"--target=${path}")
+        if "install" in command  and path:
+            command.insert(2, f"--target={path}")
         subprocess.run(command)
         
         
