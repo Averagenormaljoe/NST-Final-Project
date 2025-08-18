317186b4eeaf5c9d835e83fb33baff80bdb2c319
layo <layo_a@live.com>
2025-08-18 06:11:42 +0100

    feat: refactoring the codebase in preparation for submission

--- main_prototype/gatys_functions/compute_loss.py
+++ main_prototype/gatys_functions/compute_loss.py
@@ -1,14 +1,34 @@
+import tensorflow as tf
+
+from shared_utils.gatys_network import get_content_layer_names, get_style_layer_names
+from shared_utils.loss_functions import content_loss, style_loss, total_variation_loss
+
+
+def get_loss_layers(config):
+    loss_network = config.get("loss_network","vgg19")
+    style_names = config.get("style_layer_names", get_style_layer_names(loss_network))
+    content_names = config.get("content_layer_names", get_content_layer_names(loss_network))
+    return content_names, style_names
+def get_single_weights(config):
+    content_weight = config.get("c_weight", 1.0)
+    style_weight = config.get("s_weight", 1.0)
+    total_variation_weight = config.get("tv_weight", 1e-4)
+    return content_weight, style_weight, total_variation_weight
 def compute_loss(combination_image, base_image, style_reference_image, config = {}):
   metrics_dict = {}
   tv_type = config.get("tv_type", "gatys")
   size = config.get("size", (400, 400))
+  feature_extractor = config.get("feature", None)
+  if feature_extractor is None:
+    print("Error: feature extract has not be provided.")
+    return None, metrics_dict
   content_names, style_names = get_loss_layers(config)
   input_tensor = tf.concat(
   [base_image, style_reference_image, combination_image], axis=0)
   features = feature_extractor(input_tensor)
   loss = tf.zeros(shape=())
   w,h = size
-  content_weight_per_layer : float = single_content_weight / len(content_layer_names)
+  content_weight_per_layer : float = single_content_weight / len(content_names)
   c_loss = tf.zeros(shape=())
   # content layer iteration
   for layer_name in content_names:
---
+++ main_prototype/gatys_functions/update_model.py
@@ -0,0 +1,8 @@
+def update_model(loss_network: str, size : tuple[int,int]):
+    w,h = size
+    feature_extractor = get_model(loss_network, w,h)
+    style_layer_names = get_style_layer_names(loss_network)
+    content_layer_names = get_content_layer_names(loss_network)
+    style_weights = get_style_weights(loss_network)
+    content_weights = get_content_weights(loss_network)
+    return feature_extractor, style_layer_names, content_layer_names, style_weights, content_weights
