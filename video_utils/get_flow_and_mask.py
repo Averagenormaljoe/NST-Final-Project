b3c712d3d8956669fc87fd72066881608c1fa31c
layo <layo_a@live.com>
2025-08-22 17:19:59 +0100

    feat: updated 'get_flow_and_mask' function to use flow

--- video_utils/get_flow_and_mask.py
+++ video_utils/get_flow_and_mask.py
@@ -2,6 +2,10 @@ from video_utils.helper.get_flow_and_wrap import get_flow_and_wrap, prepare_mask
 from video_utils.mask import warp_previous_frames
 def get_flow_and_mask(config, combination_image):
     config = get_flow_and_wrap(config)
-    config = warp_previous_frames(config, combination_image=combination_image)
+    flow = config.get("flow", None)
+    if flow is None:
+        print("ERROR: flow not provided in config")
+        return config
+    config = warp_previous_frames(config,flow)
     config = prepare_mask(config, combination_image)
     return config
