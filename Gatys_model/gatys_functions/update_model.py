597ff4e2d5c4258bda29f35661ea51edba458d2b
layo <layo_a@live.com>
2025-08-22 08:46:28 +0100

    feat: renamed 'Gatys_prototype' to 'Gatys_model'

--- Gatys_model/gatys_functions/update_model.py
+++ Gatys_model/gatys_functions/update_model.py
@@ -1,4 +1,4 @@
-from Gatys_prototype.gatys_functions.get_model import get_model
+from Gatys_model.gatys_functions.get_model import get_model
 from shared_utils.gatys_network import get_content_layer_names, get_content_weights, get_style_layer_names, get_style_weights
 
 
