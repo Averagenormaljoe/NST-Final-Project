5e2d444e21cafd56c3c9e460577aad33a5a9567c
layo <layo_a@live.com>
2025-08-23 05:11:09 +0100

    feat: removed 'hyper_space.py' from the project due to being unused

--- Gatys_model/helper_functions/meta_manager.py
+++ Gatys_model/helper_functions/meta_manager.py
@@ -14,7 +14,12 @@ def prepare_metadata(config, file_paths,extra_dict):
         "optimizer": config.get('optimizer', 'adam'),
         "preserve_color": config.get('preserve_color', False),
         "noise": config.get('noise', False),
-        "clip": config.get('clip', False)
+        "clip": config.get('clip', False),
+        "lr": config.get('lr', 1.0),
+        "size": config.get('size', (400, 400)),
+        "content_weight" : config.get("c_weight", 2.5e-8),
+        "style_weight" : config.get("s_weight", 1e-6),
+        "total_variation_weight" : config.get("tv_weight", 1e-6)
     }
     if extra_dict:
         metadata.update(extra_dict)
