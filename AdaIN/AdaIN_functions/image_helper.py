9233bc44a4e77a7b70d97d45ad73a9b8e0614981
layo <layo_a@live.com>
2025-08-24 08:35:43 +0100

    feat: changed the 'get_style_images 'function in 'AdaIN/AdaIN_functions/image_helper.py' to accept image paths

--- AdaIN/AdaIN_functions/image_helper.py
+++ AdaIN/AdaIN_functions/image_helper.py
@@ -6,20 +6,15 @@ def retrieve_style_image(images_path: str):
     return style_images
 
 # Get the image file paths for the style images.
-def get_style_images(on_kaggle : bool = True, dataset : str = "artwork"):
-    if dataset == "artwork":
-        if on_kaggle:
-            style_images = retrieve_style_image("/kaggle/input/best-artworks-of-all-time/resized/resized")
-            return style_images
-        else:
-            style_images = retrieve_style_image("/content/artwork/resized")
-            return style_images
-    else:
-        style_images = retrieve_style_image("/kaggle/input/wikiart")
+def get_style_images(on_kaggle : bool = True, path : str = ""):
+        final_path = f"/kaggle/input/{path}" if on_kaggle else path
+        style_images = retrieve_style_image(final_path)
+        return style_images
+      
             
 
-def split_style_images(on_kaggle : bool, dataset : str = "artwork"):
-    style_images = get_style_images(on_kaggle, dataset)
+def split_style_images(on_kaggle : bool,path : str):
+    style_images = get_style_images(on_kaggle, path)
     total_style_images = len(style_images)
     train_split = int(0.8 * total_style_images)
     train_style = style_images[: train_split]
