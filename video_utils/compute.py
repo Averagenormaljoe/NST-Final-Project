6c44a3d6c9cf4ae82ac9be37058581b6ac63afab
layo <layo_a@live.com>
2025-08-27 06:33:41 +0100

    feat: developing functionality to disable optical flow and masks in the project using config

--- video_utils/compute.py
+++ video_utils/compute.py
@@ -2,10 +2,12 @@ import tensorflow as tf
 from video_utils.mask import long_term_temporal_loss_non_warp,temporal_loss, get_optimal_flow, warp_previous_frames
 from Ruder.luminance import  no_luminance, wrap_images_prior_luminance
 def compute_temporal_loss(combination_image, config = {}):
-    frames = config.get("warp_frames", [])
     long_term = config.get("long_term", False)
-    flow = config.get("flow", None)
-    mask = config.get("mask", None)
+    is_flow = config.get("flow", True)
+    is_mask =  config.get("mask", True) 
+    frames = config.get("warp_frames", []) if is_flow else config.get("frames",[])
+    flow = config.get("flow", None) if is_flow else None
+    mask = config.get("mask", None) if is_mask else None
     is_luminance = config.get("is_luminance ",False)
     luminance_version = config.get("is_luminance ",1)
     loss = tf.constant(0.0, dtype=tf.float32)
