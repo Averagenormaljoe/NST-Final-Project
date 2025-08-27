ecb8ae55f969aaff5f8ac9a01489abd3ca8f07c9
layo <layo_a@live.com>
2025-08-27 08:50:11 +0100

    feat: added error handling to the 'multi_pass' function

--- video_utils/mask.py
+++ video_utils/mask.py
@@ -122,14 +122,27 @@ def get_pass_range(direction : str,frames_length : int):
 # multi pass algorithm adapted from 'https://arxiv.org/abs/1604.08610' paper by Ruder et al.
 def multi_pass(n_pass : int,flows : list,style_image : tf.Tensor,masks: list, blend_weight : float =0.5,temporal_loss_after_n_passes : int = 3,config : dict = {}):
     combination_frames = config.get("frames",[])
+    
+    if not isinstance(flows, list):
+        raise TypeError(f"flows is not a list ({type(flows)}).")
+    if not isinstance(masks, list) or not all(isinstance(m, tf.Tensor) for m in masks):
+        raise TypeError(f"masks is not a list({type(masks)}).")
     if not isinstance(n_pass,int):
         print(f"Error: n_pass is not an int ({type(n_pass)}).")
+    if n_pass <= 0:
+            raise ValueError(f"n_pass is not a positive integer ({n_pass}).")
     if not isinstance(style_image,tf.Tensor):
-        print(f"Error: style_image is not a tensor ({type(style_image)}).")  
+       raise TypeError(f"Error: style_image is not a tensor ({type(style_image)}).")  
+    frames_length = len(combination_frames)
+    if frames_length == 0:
+        print("Error: frames length is zero.")
+        raise ValueError("Error: frames list is empty")
+    if not  blend_weight <= 1 and not blend_weight >= 0:
+            raise ValueError(f"Error: blend_weight is not between 0 and 1 ({blend_weight}).")
     start : float = time()
     pass_time : list = []
     stylize_frames = combination_frames.copy()
-    frames_length = len(combination_frames)
+
     neg_blend_weight = 1 - blend_weight
     loop_manager = LoopManager(config)
     for j in trange(0, n_pass, desc=f"Processing passes in multi pass algorithm"):
@@ -140,29 +153,33 @@ def multi_pass(n_pass : int,flows : list,style_image : tf.Tensor,masks: list, bl
         is_temporal_loss : bool = j >= temporal_loss_after_n_passes
         config["video_mode"] = is_temporal_loss
         for i in pass_range:
-            index_d : int = i - 1 if direction == "f" else i + 1
-            if (direction == "f" and i == 0) or (direction == "b" and i == frames_length - 1):
-                prev_img = stylize_frames[i]
-                stylize_frames[i] = prev_img
-                
-            else:
-                warp_mask = masks[index_d]
-                next_img = combination_frames[index_d]
-                reverse_flow = True if direction == "b" else False
-                warp_img = warp_flow(next_img,flows[index_d],reverse_flow)
-                first_mul = blend_weight * warp_mask * warp_img
-                ones_res = tf.ones_like(warp_mask)
-                neg_prev_mask = tf.subtract(ones_res, warp_mask) 
-                second_mul = ((neg_blend_weight * ones_res) + (blend_weight * neg_prev_mask)) * prev_img
-                final_result = tf.add(first_mul,second_mul)
-                config["combination_frame"] = final_result
-                generated_frames, best_frame, log_data = loop_manager.training_loop(content_path=combination_frames[i],  style_path=style_image,config=config)
-                if not generated_frames or not best_frame or not log_data:
-                    print("Error: optimization loop failed. Skipping")
-                    stylize_frames[i] = final_result
+            try:
+                index_d : int = i - 1 if direction == "f" else i + 1
+                if (direction == "f" and i == 0) or (direction == "b" and i == frames_length - 1):
                     prev_img = stylize_frames[i]
-                    continue
-                stylize_frames[i] = best_frame.get_image()
+                    stylize_frames[i] = prev_img
+                    
+                else:
+                    warp_mask = masks[index_d]
+                    next_img = combination_frames[index_d]
+                    reverse_flow = True if direction == "b" else False
+                    warp_img = warp_flow(next_img,flows[index_d],reverse_flow)
+                    first_mul = blend_weight * warp_mask * warp_img
+                    ones_res = tf.ones_like(warp_mask)
+                    neg_prev_mask = tf.subtract(ones_res, warp_mask) 
+                    second_mul = ((neg_blend_weight * ones_res) + (blend_weight * neg_prev_mask)) * prev_img
+                    final_result = tf.add(first_mul,second_mul)
+                    config["combination_frame"] = final_result
+                    generated_frames, best_frame, log_data = loop_manager.training_loop(content_path=combination_frames[i],  style_path=style_image,config=config)
+                    if not generated_frames or not best_frame or not log_data:
+                        print("Error: optimization loop failed. Skipping")
+                        stylize_frames[i] = final_result
+                        prev_img = stylize_frames[i]
+                        continue
+                    stylize_frames[i] = best_frame.get_image()
+                    prev_img = stylize_frames[i]
+            except Exception as e:
+                print(f"Error: during frame {i} for pass {j}: {e}")
                 prev_img = stylize_frames[i]
         pass_end : float = time()
         duration : float = pass_end - pass_tick
