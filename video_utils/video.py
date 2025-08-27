90c8f377e685cf91e462dfe1e8628b810318d66c
layo <layo_a@live.com>
2025-08-27 11:31:07 +0100

    feat: refactored 'video_utils/video.py'

--- video_utils/video.py
+++ video_utils/video.py
@@ -88,7 +88,7 @@ def save_output_video(config, video_details):
         
 
 def process_video(output_file_name,output_dir,prefix,frames_dir,frames_limit,output_fps,config):
-    output_video_path = os.path.join(output_dir,  + output_file_name)
+    output_video_path = os.path.join(output_dir, output_file_name)
     extension = config.get('extension', 'jpg')
     verbose = config.get('verbose', False)
 
@@ -99,10 +99,6 @@ def process_video(output_file_name,output_dir,prefix,frames_dir,frames_limit,out
         return
     output_frame_height, output_frame_width, _ = cv2.imread(first_output_frame).shape
     video_writer = cv2.VideoWriter(output_video_path, fourcc, output_fps, (output_frame_width, output_frame_height), True)
- 
-    if not os.path.exists(first_output_frame):
-        print(f"ERROR: First output frame not found at {first_output_frame}")
-        return
     for i in trange(frames_limit, desc="Combining the stylized frames of the video together", disable=not verbose):
         frame = cv2.imread(os.path.join(output_dir, frames_dir, f"{prefix}-{i+1:08d}.{extension}"))
         if frame is not None:
@@ -118,39 +114,26 @@ def normalize_output_size(output_size):
     if output_size is not None:
         resized_output_size = tuple(output_size) if len(output_size) > 1 else output_size[0]
         return resized_output_size
+    return None
 
 def save_frame(output_frame_path,img):
      if output_frame_path is not None and img is not None:
             keras.utils.save_img(output_frame_path, img) 
     
-        
-def video_style_transfer(config,video_details,style_func):
-    verbose = config.get('verbose', False)
-    output_size = config.get('size')
-    output_size = normalize_output_size(output_size)
-    config['size'] = output_size
-    if not video_details:
-        if verbose:
-            print("ERROR: Not details (total_frames,h,w, fps) has been provided for the video. Exiting video style transfer process.")
-        return video_details
-    total_frames,h,w, content_fps = video_details
-    content_video_path, style_path, output_dir = get_video_paths(config)
+def prepare_transferred_frames(style_func,output_dir,total_frames,style_path,config):
     frames_dir, transferred_dir, pass_dir = get_frame_dir()
     content_frames_dir = os.path.join(output_dir,  frames_dir)
     transferred_frames_dir = os.path.join(output_dir, transferred_dir)
-    pass_frames_dir = os.path.join(output_dir, pass_dir)
     create_dir(transferred_frames_dir)
     create_dir(content_frames_dir)
-    create_dir(pass_frames_dir)
     prev_frames = []
     content_prefix = config.get('content_prefix', 'frame')
     transformed_prefix = config.get('transformed_prefix', 'transferred_frame')
-    pass_prefix = config.get('pass_prefix', 'pass_frame')
     extension = config.get('extension', 'jpg')
     logs = []
     frames_limit = get_frame_limit(config, total_frames)
-    is_multi_pass = config.get("is_multi_pass",False)
-    # perform image style transfer with each content frame and style image
+    
+    verbose = config.get('verbose', False)
     for i in trange(frames_limit, desc="Performing style transfer for each frame", disable=not verbose):
         frame_i = f"{i+1:08d}"
         content_frame_path = os.path.join(content_frames_dir, f"{content_prefix}-{frame_i}.{extension}")
@@ -177,31 +160,56 @@ def video_style_transfer(config,video_details,style_func):
         prev_frames.append(appended_image)
         save_frame(output_frame_path,appended_image)
         logs.append(log_data)
-    if is_multi_pass:
-        n_pass = config.get("n_pass",3)
-        blend_weight = config.get("blend_weight", 0.5) 
-        total_flows = config.get("total_flows",[])
-        total_frames = config.get("total_frames",[])
-        temporal_loss_n = config.get("temporal_loss_n",3)
-        multi_pass_frames = multi_pass(n_pass,total_flows,style_path,blend_weight,temporal_loss_n,config=config)
-        number_of_pass_frames = len(multi_pass_frames)
-        for i in trange(number_of_pass_frames, desc="Performing multi-pass for each frame", disable=not verbose):
-            output_frame_path = os.path.join(pass_frames_dir, f"{pass_prefix}-{frame_i}.{extension}")
-            next_image =  multi_pass_frames[i]
-            save_frame(output_frame_path,next_image)
-        if verbose:
-            print("Finished multi pass step of the codebase.")
-    
-    
     if verbose:
         print("Image style transfer complete.")
         print()
         print("Synthesizing video from transferred frames...")
     config["logs"] = logs
+      
+def apply_multi_pass(config,style_path,pass_frames_dir):
+    pass_prefix = config.get('pass_prefix', 'pass_frame')
+    verbose = config.get('verbose', False)
+    extension = config.get('extension', 'jpg')
+    n_pass = config.get("n_pass",3)
+    blend_weight = config.get("blend_weight", 0.5) 
+    total_flows = config.get("total_flows",[])
+    temporal_loss_n = config.get("temporal_loss_n",3)
+    multi_pass_frames = multi_pass(n_pass,total_flows,style_path,blend_weight,temporal_loss_n,config=config)
+    number_of_pass_frames = len(multi_pass_frames)
+    for i in trange(number_of_pass_frames, desc="Performing multi-pass for each frame", disable=not verbose):
+        frame_i = f"{i+1:08d}"
+        output_frame_path = os.path.join(pass_frames_dir, f"{pass_prefix}-{frame_i}.{extension}")
+        next_image =  multi_pass_frames[i]
+        save_frame(output_frame_path,next_image)
+    if verbose:
+        print("Finished multi pass step of the codebase.")
+          
+      
+        
+def video_style_transfer(config,video_details,style_func):
+    verbose = config.get('verbose', False)
+    output_size = config.get('size')
+    output_size = normalize_output_size(output_size)
+    config['size'] = output_size
+    if not video_details:
+        if verbose:
+            print("ERROR: Not details (total_frames,h,w, fps) has been provided for the video. Exiting video style transfer process.")
+        return video_details
+    total_frames,h,w, content_fps = video_details
+    content_video_path, style_path, output_dir = get_video_paths(config)
+    frames_dir, transferred_dir, pass_dir = get_frame_dir()
+    pass_frames_dir = os.path.join(output_dir, pass_dir)
+    create_dir(pass_frames_dir)
+    
+    is_multi_pass = config.get("is_multi_pass",False)
+    # perform image style transfer with each content frame and style image
+    prepare_transferred_frames(style_func,output_dir)
+
+    if is_multi_pass:
+        apply_multi_pass(config,style_path,pass_frames_dir,total_frames,style_path,config)
+
     return video_details
-    
-    
-    
+      
     
 def execute_video_style_transfer(config, style_func):
     verbose = config.get('verbose', False)
