31d37f5989026b11efdf49fff7dcced843e83f87
layo <layo_a@live.com>
2025-09-07 12:39:19 +0100

    feat: updated 'get_flow_and_mask' function to remove unnecessary and incorrect 'warp_previous_frames' call, which previous had config pass into it

--- video_utils/get_flow_and_mask.py
+++ video_utils/get_flow_and_mask.py
@@ -1,14 +1,12 @@
 from video_utils.helper.get_flow_and_wrap import get_flow_and_wrap, prepare_mask
-from video_utils.mask import warp_previous_frames
 def get_flow_and_mask(config, combination_image):
     config = get_flow_and_wrap(config)
     flow = config.get("flow", None)
     verbose = config.get("verbose", 0)
     video_mode = config.get("video_mode",False)
-    if flow is None:
+    if flow is None or not video_mode:
         if verbose >= 1 and video_mode:
             print("ERROR: flow not provided in config")
         return config
-    config = warp_previous_frames(config,flow)
     config = prepare_mask(config, combination_image)
     return config
