ef26786c7e672a202ab9e3c9d42d7fc0429dbe92
layo <layo_a@live.com>
2025-09-08 17:24:33 +0100

    feat: removed several functions from 'shared_utils/device.py' due to them being unused

--- shared_utils/device.py
+++ shared_utils/device.py
@@ -6,27 +6,6 @@ def get_gpu_usage():
     except:
         pass
    
-
-def get_memory_usage():
-    try:
-        command = ['nvidia-smi', '--query-gpu=memory.used', '--format=csv,noheader,nounits']
-        return process_command(command)
-    except:
-        pass
-    
-def get_gpu_temperature():
-    try:
-        command = ['nvidia-smi', '--query-gpu=temperature.gpu', '--format=csv,noheader,nounits']
-        return process_command(command)
-    except:
-        pass
-def get_gpu_power_usage():
-    try:
-        command = ['nvidia-smi', '--query-gpu=power.draw', '--format=csv,noheader,nounits']
-        return process_command(command)
-    except:
-        pass
-
 def process_command(command):
     output = subprocess.check_output(command)
     return int(output.decode('utf-8').strip())
