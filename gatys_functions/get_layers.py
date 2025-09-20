4151abf5dd5a51fd2a2e0e31fc769b9cb54fbd10
layo <layo_a@live.com>
2025-09-20 19:37:28 +0100

    feat: removed the 'get_layers' function from the project

--- gatys_functions/get_layers.py
+++ gatys_functions/get_layers.py
@@ -2,16 +2,6 @@
 from shared_utils.gatys_network import get_content_layer_names, get_style_layer_names, get_style_weights, get_content_weights
 
 
-def get_layers(use_custom: bool,loss_network : str) -> tuple[list[str], list[str], dict, dict]:
-    # decides if custom layers and weights should be used
-    if use_custom:
-        style_layer_names = get_style_layer_names(loss_network)
-        content_layer_names = get_content_layer_names(loss_network)
-        style_weights = get_style_weights(loss_network)
-        content_weights = get_content_weights(loss_network)
-    else:
-        style_layer_names, content_layer_names, style_weights, content_weights = get_default_NST_layers()
-    return style_layer_names, content_layer_names, style_weights, content_weights
 
 
 def get_default_NST_layers() -> tuple[list[str], list[str], dict, dict]:
