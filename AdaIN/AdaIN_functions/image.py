9e975739b3f8d04225955702fa2b66bf6be145a5
layo <layo_a@live.com>
2025-09-21 22:53:49 +0100

    feat: changed credit for author: 'Sanan Suleymanov'

--- AdaIN/AdaIN_functions/image.py
+++ AdaIN/AdaIN_functions/image.py
@@ -2,17 +2,17 @@ from video_utils.video_helper import image_read
 import cv2
 import numpy as np
 import tensorflow as tf
+# Code from https://medium.com/@sanansuleyman/style-video-with-neural-style-transfer-d10a35cf0e3
+# Website: Medium
+# Title: Style Video with Neural Style Transfer
+# Author: Sanan Suleymanov
+# Date: Jan 28, 2023
 def load_image(path : str):
     style_im = cv2.imread(path)
     style_im = cv2.cvtColor(style_im, cv2.COLOR_BGR2RGB)
     style_im = image_read(style_im)
     return style_im
 
-# Code from https://medium.com/@sanansuleyman/style-video-with-neural-style-transfer-d10a35cf0e3
-# Website: Medium
-# Title: Style Video with Neural Style Transfer
-# Author: Sanan Suleymanov
-# Date: Jan 28, 2023
 def tensor_toimage(tensor : tf.Tensor):
   tensor =tensor*255
   tensor = np.array(tensor, dtype=np.uint8)
