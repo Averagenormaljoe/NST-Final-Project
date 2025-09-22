71c02d6b4a8c7b514a844bf5e83bde2d682e5937
layo <layo_a@live.com>
2025-09-22 13:44:03 +0100

    feat: added code for displaying results in the project

--- helper_functions/training_helper.py
+++ helper_functions/training_helper.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import keras
 import numpy as np
 import os
+from IPython.display import Image, display
 def result_save(content_name : str,style_name: str,iterations : int, img: np.ndarray,verbose: int = 0) -> None:
     now = datetime.now()
     time_format : str = "%Y%m%d_%H%M%S"
@@ -12,5 +13,7 @@ def result_save(content_name : str,style_name: str,iterations : int, img: np.nda
     file_extension = ".png"
     fname = f"{output_dir}/{content_name}_{style_name}_{now}_{data_name}_{iterations}.{file_extension}"
     keras.utils.save_img(fname, img) 
+    
     if verbose > 0:
         print(f"Image saved at iteration {iterations}")
+        display(Image(filename=fname))
